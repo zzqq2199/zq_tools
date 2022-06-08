bf8fb240461493d5d113c3f8c15e8cc26e0efee9
zzqq2199 <zhouquanjs@qq.com>
2022-06-08 13:47:31 +0800

    add long_descriptions and classifiers

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 import setuptools
 
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="zq-tools",
     version="0.2.0",
     author="zzqq2199",
     author_email="zhouquanjs@qq.com",
     description="A collection of tools for zzqq2199",
-    packages=["zq_tools"]
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["zq_tools"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
